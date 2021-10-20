6cc9bbc317443a787dea0be7e7c42c981f811634
Robert Kleniewski <robert.kleniewski@pl.ibm.com>
2021-10-20 20:21:00 +0200

    switching to scss and load Carbon

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.scss';
+// Carbon COmponents
+import { Button } from 'carbon-components-react';
+
+function App() {
+  return (
+    <div className="App">
+      <Button>IBM Carbon components loads OK</Button>
+    </div>
+  );
+}
+
+export default App;
